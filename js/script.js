a1846bca0fa37cf0e51f8d76e069e92cd883270f
LyamovSergey <lyamofff@gmail.com>
2018-09-20 01:30:47 +0300

    large size

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 $(function() {
-    
 
+
+   $('.card').hover(function(){
+        $(this).attr('id', 'mouseOn')
+        .height('auto');
+
+    },function(){
+        $(this).removeAttr('id');
+    }); 
+
+
+
+   $('.optionOn').on('click', function(){
+    $(this).addClass('active-option')
+    .siblings()
+    .removeClass('active-option');
+   });
+
+
+   $('.card__colors-block div').on('click', function(){
+    $(this).addClass('activeColor')
+    .siblings()
+    .removeClass('activeColor');
+    if($(this).hasClass('pink')){
+        $('.card__phone').attr('src', 'img/mainPhone2.jpg');
+    }else{
+        $('.card__phone').attr('src', 'img/mainPhone.jpg');
+    };
+   });
 });
+
